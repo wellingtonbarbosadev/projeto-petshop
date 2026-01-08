a8adf66dd6170fd52bb315fb3f3c674df8ecd3c7
Wellington Barbosa <huehueb58@gmail.com>
2026-01-08 13:56:00 -0300

    adiciona campos obrigatórios no modal de agendamento e melhora estilos de interação

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,6 @@
 const openModal = document.querySelector(".open-modal")
 const modal = document.querySelector("#modal-1")
 
-modal.showModal()
-
 openModal.addEventListener("click", ((e) => {
   const modalId = openModal.getAttribute("data-modal")
   const modal = document.getElementById(modalId)
